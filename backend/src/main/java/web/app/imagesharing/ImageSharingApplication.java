f2c91569479d5dec84c3381b8da99f517d1738d8
ivanpang <ivanpang1996@gmail.com>
2021-09-04 22:00:46 +0800

    init project

---
+++ backend/src/main/java/web/app/imagesharing/ImageSharingApplication.java
@@ -0,0 +1,13 @@
+package web.app.imagesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageSharingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/web/app/imagesharing/ImageSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package web.app.imagesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
