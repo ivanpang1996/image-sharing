67d6610a01b547a9df55587a4e77df7aefb20346
ivanpang <ivanpang1996@gmail.com>
2021-09-10 01:22:04 +0800

    login post api

--- backend/src/main/java/web/app/imagesharing/login/Login.java
+++ backend/src/main/java/web/app/imagesharing/login/Login.java
@@ -1,11 +1,11 @@
 package web.app.imagesharing.login;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Login {
-    @GetMapping("/api/login")
+    @PostMapping("/api/login")
     public LoginResponse login() {
         var response = new LoginResponse();
         response.aaa = "successsss";
--- frontend/src/components/loginForm/LoginForm.tsx
+++ frontend/src/components/loginForm/LoginForm.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import {Form} from 'react-bootstrap';
+import {Button, Form} from 'react-bootstrap';
+import axios from "axios";
 
 function LoginForm() {
+
+    const login = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+        alert(event.target)
+
+        fetch('/api/login', {
+            method: 'POST',
+            body: null,
+        }).then(res => res.json())
+            .then(
+                (response) => {
+                    alert(response.aaa);
+                },
+                (error) => {
+                    alert("error")
+                }
+            );
+    }
+
+
+    //
+    // const login = async (data: string) => {
+    //     const success = await axios.put("/api/login", data);
+    //     success ? alert("Profile successfully updated!") : alert("Error updating profile. Please try again later.");
+    // };
+
     return (
         <div className="d-flex justify-content-center">
-            <Form>
+            <Form method="post" onSubmit={login}>
                 <Form.Group className="mb-3" controlId="formGroupEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email"/>
@@ -13,8 +41,10 @@ function LoginForm() {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password"/>
                 </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Form>
-
         </div>
     );
 }
